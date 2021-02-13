940d562e0bd0bad92f07050364ff2639e840da36
Adem Çelik <clk.ademm@gmail.com>
2021-02-13 22:48:06 +0300

    İlk commit

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        //naming convention
+        //IoC container -- İnversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+             var result = productService.GetAll();
+            return result.Data;
+        
+        }
+
+    }
+}
