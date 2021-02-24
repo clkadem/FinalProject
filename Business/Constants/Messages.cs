643fb272773726d0bcde9c18858f57d169cc48af
Adem Çelik <clk.ademm@gmail.com>
2021-02-25 00:04:43 +0300

    13.Gün Sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Bakım zamanı";
         public static string ProductsListed = "Ürünler Listelendi";
+        public static string ProductCountOfCategoryError = "Bu kategoride en Fazla 10 ürün bulunabilir";
+        public static string ProductNameAlreadyExistts = "Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceted = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
